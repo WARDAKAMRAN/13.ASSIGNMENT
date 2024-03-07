43c029564a4e5fda3361ba704641bb85269ef93a
Warda Kamran <wkamran523@gmail.com>
2024-03-07 07:39:26 -0800

    13.AASIGNMENT

---
+++ main.js
@@ -0,0 +1,6 @@
+// exerices 13
+var transportation = ['civics', 'bike', 'bus', 'mehran'
+];
+for (var i = 0; i < transportation.length; i++) {
+    console.log(' I would like to own a' + transportation[i]);
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+// exerices 13
+let transportation : string [] = ['civics','bike','bus','mehran' 
+];
+for(var i=0; i<transportation.length; i++){
+    console.log(' I would like to own a' + transportation[i]);
+
+}
